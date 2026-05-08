f7137588309ba85089a7a728f9ae606c7e71a05a
Younes Darabi <younes.darabi@outlook.com>
2026-05-08 16:01:50 +0200

    add login

--- apps/authentication/api/urls.py
+++ apps/authentication/api/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-from .views import RegisterView, LogoutView, HelloView
+from .views import RegisterView, LogoutView, LoginView, TokenRefreshView, HelloView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='Login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('hello/', HelloView.as_view(), name='hello'),
 ]
--- apps/authentication/api/views.py
+++ apps/authentication/api/views.py
@@ -1,5 +1,7 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.views import APIView, Response, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
 
 from .serializers import RegisterSerializer
 
@@ -12,7 +14,7 @@ class RegisterView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'detail':'User created successfully!'}, status=status.HTTP_201_CREATED)
+            return Response({'detail': 'User created successfully!'}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -25,4 +27,45 @@ class HelloView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response({'message':'Hello World'})
+        return Response({'message': 'Hello World'})
+
+
+class LoginView(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        refresh = response.data.get('refresh')
+        access = response.data.get('access')
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+
+        response.set_cookie(
+            key="access_token",
+            value=access,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+
+        username = request.data.get('username')
+        user = User.objects.get(username=username)
+
+        response.data = {
+            "detail": "Login successfully!",
+            "user": {
+                "id": user.pk,
+                "username": user.username,
+                "email": user.email
+            }
+        }
+        return response
+
+
+class TokenRefreshView(TokenRefreshView):
+    pass
