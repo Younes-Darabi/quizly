f9f1c025ad5cc5ae6c173f1dcc7b7ed4f3e5dcc8
Younes Darabi <younes.darabi@outlook.com>
2026-05-08 16:02:22 +0200

    add: token refresh

--- apps/authentication/api/views.py
+++ apps/authentication/api/views.py
@@ -68,4 +68,38 @@ class LoginView(TokenObtainPairView):
 
 
 class TokenRefreshView(TokenRefreshView):
-    pass
+
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh_token')
+
+        if refresh_token is None:
+            return Response(
+                {'Detail': 'Refresh token not found!'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = self.get_serializer(data={'refresh': refresh_token})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response(
+                {'Detail': 'Refresh token invalid!'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        access_token = serializer.validated_data.get('access')
+
+        response = Response(
+            {"detail": "Token refreshed"}
+        )
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+
+        return response
