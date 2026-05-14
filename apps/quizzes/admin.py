b65d94dc4862fdff45ffe34efc285bc0014c4069
Younes Darabi <younes.darabi@outlook.com>
2026-05-14 19:00:58 +0200

    add admin panel

--- apps/quizzes/admin.py
+++ apps/quizzes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quiz
+
+
+class QuizAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'title', 'description', 'user', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'description', 'user__username', 'user__email']
+    ordering = ['-created_at']
+
+
+admin.site.register(Quiz, QuizAdmin)
